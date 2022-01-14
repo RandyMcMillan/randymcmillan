6fc826141c80688e374ea200ba7660cd4188778e
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-01-14 12:32:48 -0500

    modified:   block_request.py

--- block_request.py
+++ block_request.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
-import requests
+import json, requests
+from requests import Request, Session
+from django.http import HttpResponse
 
-base_url = 'https://mempool.space/api/blocks'
+
+base_url = 'https://mempool.space/api/blocks/'
 first_block = 700000
-last_block = 700001
+last_block = 700020
 
 for i in range(first_block, last_block):
-
-    headers = {'Authorization': 'Bearer Variable1', }
+    headers = {'Authorization': 'mempool mempool', }
     files = [('server', '*'), ]
-
     url = ''.join([base_url, str(i)])
-    res = requests.put(url, headers=headers, files=files, verify=True)
+    #res = requests.get(url, headers=headers, files=files, verify=True)
+    r = Request('GET', url, headers=headers, files=files)
+    prepped = r.prepare()
+    #print(prepped.url)
+    #print(prepped.headers)
+    s = Session()
+    res = s.send(prepped)
+    #print(res.status_code)
+    #print(res.json)
+    mempoolDATA = json.loads(str(res.text))
+    print(mempoolDATA)

8d08e023b25f831576431efbf0a73047f96cb0ae
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-16 17:53:31 -0500

    andymcmillan.twitter.py: add TwitterAPI tweet block_height

--- randymcmillan.twitter.py
+++ randymcmillan.twitter.py
@@ -1,49 +1,86 @@
 #!/usr/bin/env python3
-
+# https://github.com/geduldig/TwitterAPI
 import sys
 import os
+import shutil
 import importlib as imp
 from importlib.resources import read_text
+import time
+import blockcypher
 os.environ['PYTHONPATH']
 sys.path.append('.')
-# sys.path.append('./twitter')
-# sys.path.append('./twitter/build')
 sys.path.append("/usr/local/lib/python3.7/site-packages")
-# sys.path.append("/usr/local/lib/python3.8/site-packages")
-# sys.path.append("/usr/local/lib/python3.9/site-packages")
 from TwitterAPI import TwitterAPI
-# help()
 
-ACCESS_TOKEN_SECRET = os.path.expanduser('./twitter_access_tokens/access_token_secret.txt')
-ACCESS_TOKEN = os.path.expanduser('./twitter_access_tokens/access_token.txt')
-CONSUMER_API_KEY = os.path.expanduser('./twitter_access_tokens/consumer_api_key.txt')
-CONSUMER_API_SECRET_KEY = os.path.expanduser('./twitter_access_tokens/consumer_api_secret_key.txt')
-print(ACCESS_TOKEN_SECRET)
+millis = int(round(time.time() * 1000))
+seconds = int(round(time.time()))
 
+print(os.getcwd())
 
-def getCredentials(filename):
+try:
+    # shutil.move(os.path.join(filedir, "pages.html"), os.getcwd())
+    shutil.move(os.getcwd()+"/BLOCK_TIME", os.getcwd()+"/OLD_BLOCK_TIME")
+except:
+    f = open("BLOCK_TIME", "w")
+    f.write("" + 0 + "\n")
+    f.close()
+
+try:
+    block_time = blockcypher.get_latest_block_height(coin_symbol='btc')
+    block_height = repr(block_time)
+    f = open("BLOCK_TIME", "w")
+    f.write("" + block_height + "\n")
+    f.close()
+    # print(block_time)
+    print(block_height)
+except:
+    block_time = 0
+    pass
+
+def getData(filename):
     f = open(filename)
     global data
     data = f.read()
     f.close()
     return data
 
-cask = getCredentials(CONSUMER_API_SECRET_KEY)
-print(cask)
-cak  = getCredentials(CONSUMER_API_KEY)
-print(cak)
-ats  = getCredentials(ACCESS_TOKEN_SECRET)
-print(ats)
-at   = getCredentials(ACCESS_TOKEN)
-print(at)
+# def tweetBlockTime():
+#     r = api.request('statuses/update', {'status': block_height })
+#     if (r.status_code == 200):
+#         print('SUCCESS')
+#     else:
+#         print('FAILURE')
 
-# https://github.com/geduldig/TwitterAPI
-api = TwitterAPI(
-    cak,
-    cask,
-    at,
-    ats
-)
-r = api.request('search/tweets', {'q':'bitcoin'})
-for item in r:
-        print(item)
+def tweetBlockTime():
+    print(block_height)
+    print(obt)
+    if (block_height != obt):
+        r = api.request('statuses/update', {'status': block_height })
+        if (r.status_code == 200):
+            print('api.request SUCCESS')
+        else:
+            print('api.request FAILURE')
+    else:
+        print('tweetBlockTime() FAILURE')
+
+OLD_BLOCK_TIME = os.path.expanduser('./OLD_BLOCK_TIME')
+
+ACCESS_TOKEN_SECRET = os.path.expanduser('./twitter_access_tokens/access_token_secret.txt')
+ACCESS_TOKEN = os.path.expanduser('./twitter_access_tokens/access_token.txt')
+CONSUMER_API_KEY = os.path.expanduser('./twitter_access_tokens/consumer_api_key.txt')
+CONSUMER_API_SECRET_KEY = os.path.expanduser('./twitter_access_tokens/consumer_api_secret_key.txt')
+# print(ACCESS_TOKEN_SECRET)
+
+cak  = getData(CONSUMER_API_KEY)
+cask = getData(CONSUMER_API_SECRET_KEY)
+at   = getData(ACCESS_TOKEN)
+ats  = getData(ACCESS_TOKEN_SECRET)
+obt  = getData(OLD_BLOCK_TIME)
+
+api  = TwitterAPI(cak,cask,at,ats)
+
+# r = api.request('search/tweets', {'q':'bitcoin'})
+# for item in r:
+#         print(item)
+
+tweetBlockTime()

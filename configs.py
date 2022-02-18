de737f957e53bad9d0c964fbc1f7c321d08dce6b
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-02-17 20:10:05 -0500

    config.py

--- configs.py
+++ configs.py
@@ -14,14 +14,15 @@ IS_LINUX   = psutil.LINUX
 IS_WINDOWS = psutil.WINDOWS
 PATH = getcwd()
 
-def sys_info():
+def sys_info(args):
 
-    print("sys.path:%s",sys.path)
-    print(sys.path)
-    print("Python version")
-    print (sys.version)
-    print("Version info.")
-    print (sys.version_info)
+    if args.verbose:
+        print("sys.path:%s\n",sys.path)
+        # print(sys.path)
+        # print("Python version")
+        # print (sys.version)
+        # print("Version info.")
+        # print (sys.version_info)
 
 def insertPaths():
 
@@ -56,26 +57,6 @@ TEST                = False
 REPORT              = True
 KEY_REPORT          = True
 SLEEP_REPORT        = True # uses SLEEP_AMOUNT to pause report
-
-PROFILE             = False
-#usage
-#check imports.py
-#import cProfile, pstats, io
-#from pstats import SortKey
-
-#cProfile Usage
-#if (PROFILE):
-    #pr = cProfile.Profile()
-    #pr.enable()
-
-    # ... do something ...
-
-    #pr.disable()
-    #s = io.StringIO()
-    #sortby = SortKey.CUMULATIVE
-    #ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    #ps.print_stats()
-    #print(s.getvalue())
 
 NOTIFY              = True
 TIME_OUT            = 5
@@ -112,28 +93,19 @@ if __name__ == "__main__":
     from sys        import *
     import argparse
     insertPaths()
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser('config')
     parser.add_argument("-arg1", action="store_true", help="-arg1 example")
     parser.add_argument("-arg2", action="store_true", help="-arg2 example")
-
-    int_group = parser.add_argument_group('integer args')
-    int_group.add_argument("-x", action="store_true", help="x arg example")
-    int_group.add_argument("-y", action="store_true", help="y arg example")
-
-    mutex_group = parser.add_mutually_exclusive_group()
-    mutex_group.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-    mutex_group.add_argument("-q", "--quiet", action="store_true")
-
-    subparsers = parser.add_subparsers(title='sys_info', description='sys info commands', help='get sys info')
-    parser_sys_info = subparsers.add_parser('sys_info')
-    parser_sys_info.add_argument("sys_info", action="store_true", help="sys_info example")
-    parser_sys_info.add_argument("-sys1", action="store_true", help="-sys1 example")
-    parser_sys_info.add_argument("-sys2", action="store_true", help="-sys2 example")
-    parser_sys_info.set_defaults(func=sys_info)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-q", "--quiet", action="store_true")
 
     args = parser.parse_args()
     if args.verbose:
         logger.info("args.verbose = %s",args.verbose)
+        sys_info(args)
+        pass
+    if args.quiet:
+        logger.info("args.quiet = %s",args.quiet)
         pass
     if args.arg1:
         logger.info("args.arg1 = %s",args.arg1)
@@ -141,14 +113,4 @@ if __name__ == "__main__":
     if args.arg2:
         logger.info("args.arg2 = %s",args.arg2)
         pass
-
-#    args = parser_sys_info.parse_args()
-
-    if args.sys_info:
-        if args.sys1:
-            logger.info("args.sys1 = %s",args.sys1)
-            pass
-        if args.sys2:
-            logger.info("args.sys2 = %s",args.sys2)
-            pass
 

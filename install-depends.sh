a7709674a032abb42897c52f2f6936447bac9152
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-02-04 12:10:56 -0500

    install-depends.sh:

--- install-depends.sh
+++ install-depends.sh
@@ -4,43 +4,105 @@ OS=$(uname)
 OS_VERSION=$(uname -r)
 UNAME_M=$(uname -m)
 ARCH=$(uname -m)
+AWK=$(which awk)
+GAWK=$(which gawk)
 export OS
 export OS_VERSION
 export UNAME_M
 export ARCH
+export AWK
+export GAWK
+
+if [[ ! -z "$GAWK" ]]; then
+    AWK=$(which gawk)
+    export AWK
+    echo $AWK
+fi
+
+export PATH=$PATH:/usr/local/opt/python@${python_version_major}.${python_version_minor}/Frameworks/Python.framework/Versions/${python_version_major}.${python_version_minor}/bin
 
 report() {
 echo OS:
-echo "$OS" | awk '{print tolower($0)}'
+echo "$OS" | "$AWK" '{print tolower($0)}'
 echo OS_VERSION:
-echo "$OS_VERSION" | awk '{print tolower($0)}'
+echo "$OS_VERSION" | "$AWK" '{print tolower($0)}'
 echo UNAME_M:
-echo "$UNAME_M" | awk '{print tolower($0)}'
+echo "$UNAME_M" | "$AWK" '{print tolower($0)}'
 echo ARCH:
-echo "$ARCH" | awk '{print tolower($0)}'
+echo "$ARCH" | "$AWK" '{print tolower($0)}'
 echo OSTYPE:
-echo "$OSTYPE" | awk '{print tolower($0)}'
+echo "$OSTYPE" | "$AWK" '{print tolower($0)}'
 }
 
 checkbrew() {
 
-    if hash brew 2>/dev/null; then
-        brew install awk git
-        brew install --cask keybase
-        brew install python@3
-        $(PYTHON3) -m pip install -r requirements.txt
-        brew install sphinx-doc
-        brew link --force sphinx-doc
-        echo
-        brew install tccutil
-        curl -k -o /usr/local/bin/dockutil https://raw.githubusercontent.com/kcrawford/dockutil/master/scripts/dockutil
-        chmod a+x /usr/local/bin/dockutil
-        dockutil --add '/Applications/Keybase.app' --replacing 'Keybase'
+arch_name="$(uname -m)"
 
+if [ "${arch_name}" = "x86_64" ]; then
+    if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
+        echo "Running on Rosetta 2"
     else
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-        checkbrew
+        echo "Running on native Intel"
     fi
+elif [ "${arch_name}" = "arm64" ]; then
+    echo "Running on arm64"
+elif [ "${arch_name}" = "aarch64" ]; then
+    echo "Running on aarch64"
+else
+    echo "Unknown architecture: ${arch_name}"
+fi
+
+if hash apt-get 2>/dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y runc containerd
+    sudo apt-get install -y \
+    python3 python3-pip mkdocs \
+    jq runc containerd docker.io docker-compose \
+        mkdocs
+    if ! hash pip3 2>/dev/null; then
+	sudo apt-get install python3-pip
+    else
+        pip3 install omegaconf glom
+    fi
+exit
+fi
+
+if hash brew 2>/dev/null; then
+    if ! hash gawk 2>/dev/null; then
+        brew install gawk
+    fi
+    #brew install python@3.8
+    if ! hash docker 2>/dev/null; then
+        brew install --cask docker
+        brew install docker
+        brew link docker
+    fi
+    if ! hash docker-compose 2>/dev/null; then
+        brew install docker-compose
+    fi
+    if ! hash mkdocs 2>/dev/null; then
+        brew install mkdocs
+    fi
+    if ! hash jq 2>/dev/null; then
+        brew install jq
+    fi
+    brew install --quiet  --force python@3.9
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    sudo -H python3 get-pip.py --no-warn-script-location
+
+    if [ -f "/usr/local/opt/python@3.9/bin/python3" ] && [ -f "/opt/homebrew/opt/python@3.9/bin/python3" ]; then
+        python39=/opt/homebrew/opt/python@3.9/bin/python3
+    else
+        python39=$(which python3)
+    fi
+    export python39
+    $python39 -m pip -q install --upgrade pip
+    $python39 -m pip -q install omegaconf
+    echo
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    checkbrew
+fi
 }
 checkraspi(){
 
@@ -67,35 +129,39 @@ if [[ "$OSTYPE" == "linux"* ]]; then
     #CHECK APT
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         if hash apt 2>/dev/null; then
-            apt install awk
+            sudo apt install "$AWK"
+            sudo apt update
+            sudo apt install software-properties-common
+            sudo apt install python3
+            sudo apt-get install python3-pip
+            sudo apt install runc containerd docker.io docker-compose
+
             report
             echo 'Using apt...'
-            curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
-            sudo apt install ./keybase_amd64.deb
-            run_keybase
-            sudo apt install  libappindicator1 libdbusmenu-gtk4 libindicator7 indicator-application #python-setuptools #keybase
-            $(PYTHON3) -m pip install -r requirements.txt
         fi
     fi
     if [[ "$OSTYPE" == "linux-musl" ]]; then
         if hash apk 2>/dev/null; then
-            apk add awk
+            apk add "$AWK"
             report
             echo 'Using apk...'
-            apk add keybase-client
+            apk add python3 py3-pip
+            python3 -m pip install -r requirements.txt || \
+            python3 -m pip install -r ../requirements.txt
+
+
         fi
     fi
     if [[ "$OSTYPE" == "linux-arm"* ]]; then
         checkraspi
         if hash apt 2>/dev/null; then
-            apt install awk
+            apt install "$AWK"
             report
             echo 'Using apt...'
-            echo https://lekstu.ga/posts/keybase-rpi/
+
         fi
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    $(PYTHON3) -m pip install -r requirements.txt
     checkbrew
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo TODO add support for $OSTYPE
